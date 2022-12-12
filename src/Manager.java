5bc15c83d2b444da49c9b40df67be3c907a80078
Woogie95 <choisw950928@gmail.com>
2022-12-12 21:35:32 +0900

    feat: 관리자

--- src/Manager.java
+++ src/Manager.java
@@ -2,6 +2,15 @@ public class Manager {
 
     public void startCalculator() {
 
+        String input = Input.input();
+        Expression expression = new Expression(input); // 문자
+        Arithmetic arithmetic = new Arithmetic(input, expression.getSymbols()); // 숫자
+
+        System.out.println("입력된 숫자 : " + arithmetic.getNumbers());
+        System.out.println("입력된 연산자 : " + expression.getSymbols());
+
+        System.out.println();
+        Output.printResult(expression, arithmetic);
     }
 
 }
