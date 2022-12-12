9bf9f81fb274def76c36b06805cc5bafc6026284
Woogie95 <choisw950928@gmail.com>
2022-12-12 21:35:20 +0900

    refactor: new 를 통해 생성

--- src/Input.java
+++ src/Input.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 
 public class Input {
 
-    private static final Scanner SCANNER = new Scanner(System.in);
-
     public static String input() {
-        System.out.println("계산할 값을 입력해주세요.");
-        return SCANNER.nextLine();
+        System.out.println("계산할 식을 입력해주세요.");
+        return new Scanner(System.in).next();
     }
 
 }
