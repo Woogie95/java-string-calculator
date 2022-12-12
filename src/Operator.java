08f97337ce4215c66a91e24d9e0045fea4c17a56
Woogie95 <choisw950928@gmail.com>
2022-12-12 21:35:47 +0900

    feat: 연산자 연관된 상수

--- src/Operator.java
+++ src/Operator.java
@@ -1,2 +1,18 @@
-public class Operator {
+public enum Operator {
+
+    PLUS("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+
+    private final String symbol;
+
+    Operator(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
 }
