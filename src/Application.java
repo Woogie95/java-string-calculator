9942e634cc6c1d0d09f6ed76701daf3ffea37bf9
Woogie95 <choisw950928@gmail.com>
2022-12-27 14:28:06 +0900

    docs: again

--- src/Application.java
+++ src/Application.java
@@ -2,14 +2,6 @@
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            Manager manager = new Manager();
-            manager.startCalculator();
-        } catch (IllegalArgumentException msg) {
-            System.out.println(msg.getMessage());
-        } catch (Exception msg) {
-            System.out.println("예기치 못한 예외가 발생하였습니다.");
-        }
-    }
 
+    }
 }
